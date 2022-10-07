1048943a9f3a68349cbb0ed56e090957a251ab0d
andkain <andke1462@gmail.com>
2022-10-06 21:00:48 -0500

    agrego archivos

---
+++ src/main/java/com/example/demo/Demoandkain3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoandkain3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoandkain3Application.class, args);
+	}
+
+}
